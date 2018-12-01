6a07cecfec1f26fe960018df474e42fcad942ff7
Vineesh VK <vineesh27vk@gmail.com>
2018-12-01 16:41:27 +0530

    Authentication working

--- src/schema/Resolvers.ts
+++ src/schema/Resolvers.ts
@@ -1,15 +1,39 @@
+import { User } from '../entity/User';
+import { hash, compare } from 'bcrypt';
+
 const Resolver = {
 	Query: {
 		test: _ => 'testing'
 	},
 	Mutation: {
-		register: (_, { email, password }) => {
-			return { id: '1234', admin: true };
+		register: async (_, { email, password, admin=false }) => {
+			const userExist = await checkUserExists(email);
+			if (userExist) return null;
+
+			const hashedPassword = await hash(password, 10);
+			const user = await User.create({
+				email,
+				password: hashedPassword,
+				admin
+			}).save();
+
+			return { id: user.id, admin: user.admin };
 		},
-		login: (_, { email, password }) => {
-			return { id: '1234', admin: true };
+		login: async (_, { email, password }) => {
+			const userExist = await checkUserExists(email);
+			if (!userExist) return null;
+
+			const validPassword = await compare(password, userExist.password);
+			if (!validPassword) return null;
+
+			return { id: userExist.id, admin: userExist.admin };
 		}
 	}
 };
+
+async function checkUserExists(email: string) {
+	const userExist = await User.findOne({ email });
+	return userExist;
+}
 
 export default Resolver;
