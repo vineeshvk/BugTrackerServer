a1a68e75c30b6411887c09614fb0ee1efb715c00
vineesh27vk <vineesh27vk@gmail.com>
2018-12-13 10:55:01 +0530

    updated ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,25 +1,24 @@
-// const production = {
-// 	type: 'postgres',
-// 	host: 'postgres',
-// 	port: 5432,
-// 	username: 'postgres',
-// 	password: '12345',
-// 	database: 'postgresdb',
-// 	synchronize: true,
-// 	logging: false,
-// 	entities: ['src/entity/**/*.ts'],
-// 	migrations: ['src/migration/**/*.ts'],
-// 	subscribers: ['src/subscriber/**/*.ts'],
-// 	cli: {
-// 		entitiesDir: 'src/entity',
-// 		migrationsDir: 'src/migration',
-// 		subscribersDir: 'src/subscriber'
-// 	}
-// };
-
 console.log(process.env.DATABASE_URL);
+const docker = {
+	type: 'postgres',
+	host: 'postgres',
+	port: 5432,
+	username: 'postgres',
+	password: '12345',
+	database: 'postgresdb',
+	synchronize: true,
+	logging: false,
+	entities: ['src/entity/**/*.ts'],
+	migrations: ['src/migration/**/*.ts'],
+	subscribers: ['src/subscriber/**/*.ts'],
+	cli: {
+		entitiesDir: 'src/entity',
+		migrationsDir: 'src/migration',
+		subscribersDir: 'src/subscriber'
+	}
+};
 
-const production = {
+const deployed = {
 	url: process.env.DATABASE_URL,
 	type: 'postgres',
 	synchronize: true,
@@ -36,4 +35,4 @@ const production = {
 		ssl: true
 	}
 };
-module.exports = production;
+module.exports = process.env.DATABASE_URL ? deployed : docker;
